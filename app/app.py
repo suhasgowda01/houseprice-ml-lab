bdf61ff39d70c18f509a81e7d48ba999f1585e95
Hemantha <hemanthas6361@gmail.com>
2025-09-11 13:08:42 +0530

    sept11

--- app/app.py
+++ app/app.py
@@ -4,23 +4,27 @@ import pandas as pd
 
 st.title("🏠 House Price Predictor")
 
-model = joblib.load("model.pkl")
+model = joblib.load("../model.pkl")
 
 with st.form("predict"):
     rent = st.number_input("Monthly Rent", value=15000)
-    sqft = st.number_input("Square Feet", value=1000)
+    area = st.number_input("Square Feet", value=1000)
     locality = st.text_input("Locality (e.g., Downtown)", "Downtown")
-    bedrooms = st.number_input("Bedrooms", value=2, min_value=0, max_value=10, step=1)
+    bathrooms = st.number_input("bathrooms", value=2, min_value=0, max_value=10, step=1)
     parking = st.text_input("Parking (Open/Covered/Missing)", "Open")
+    BHK = st.number_input("BHK")
+    facing = st.text_input("facing")
     submitted = st.form_submit_button("Predict")
 
 if submitted:
     X = pd.DataFrame([{
         "rent": rent,
-        "sqft": sqft,
+        "area": area,
         "locality": locality if locality else "Missing",
-        "bedrooms": bedrooms,
-        "parking": parking if parking else "Missing"
+        "bathrooms": bathrooms,
+        "parking": parking if parking else "Missing",
+        "BHK" : BHK,
+        "facing" : facing
     }])
     pred = model.predict(X)[0]
     st.success(f"Predicted Price: {pred:,.2f}")
